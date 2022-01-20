7334171ee1bab2d707d7b02159591c73066b0dd9
Kaustubh Nawade <nawadk@amazon.com>
2022-01-20 13:43:00 +0530

    added linked list queue

--- src/main/kotlin/linkedList/LinkedList.kt
+++ src/main/kotlin/linkedList/LinkedList.kt
@@ -4,9 +4,9 @@ package linkedList
  * Linked List
  */
 class LinkedList<T> {
-    private var head: Node<T>? = null
-    private var tail: Node<T>? = null
-    private var size = 0
+    var head: Node<T>? = null
+    var tail: Node<T>? = null
+    var size = 0
 
     fun isEmpty(): Boolean {
         return size == 0
--- src/main/kotlin/queue/LinkedListQueue.kt
+++ src/main/kotlin/queue/LinkedListQueue.kt
@@ -1,5 +1,24 @@
-package queue//class LinkedListQueue<T> : Queue.Queue<T> {
-//    private val list = Double
-//
-//
-//}
+package queue
+
+import linkedList.LinkedList
+
+class LinkedListQueue<T> : Queue<T> {
+
+    private val list = LinkedList<T>()
+
+    override val count: Int
+        get() = list.size
+
+    override fun peek(): T? {
+        return list.head?.value
+    }
+
+    override fun enqueue(element: T): Boolean {
+        list.append(element)
+        return true
+    }
+
+    override fun dequeue(): T? {
+        return list.pop()
+    }
+}
